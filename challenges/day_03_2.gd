cf12d2a18260e278613c04838f68f39b8441e2d3
graydoubt <graydoubt@graydoubt.com>
2024-12-05 11:16:21 -0500

    feat: day 3 challenge solutions

---
+++ challenges/day_03_2.gd
@@ -0,0 +1,54 @@
+class_name AOC2024Day3Part2 extends CanvasLayer
+
+## Advent of Code 2024 - Day 3 - Part 2
+##
+## Please see the problem description:
+## [url=https://adventofcode.com/2024/day/3]Day 3: Mull It Over[/url]
+## [br]
+## [br]
+## My solution takes this approach:[br]
+## - moar regular expressions[br]
+
+
+@export var input_edit: TextEdit
+
+@export var results_button: Button:
+	set(value):
+		results_button = value
+		results_button.pressed.connect(_on_results_button_pressed)
+
+@export var results_label: Label
+
+
+func _on_results_button_pressed() -> void:
+	
+	var regex: RegEx = RegEx.new()
+	regex.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)|do\\(\\)|don't\\(\\)")
+	var matches = regex.search_all(input_edit.text)
+	
+	var total: int = 0
+	var enabled: bool = true
+	for m in matches:
+		#print(m.strings)
+		# Casting to an Array[String] because match doesn't work with RegExMatch's strings,
+		# which is a PackedStringArray.
+		var array: Array[String]
+		array.assign(m.strings)
+		
+		match array:
+			["do()", ..]:
+				enabled = true
+			["don't()", ..]:
+				enabled = false
+			_:
+				if enabled:
+					total += mul(int(m.strings[1]), int(m.strings[2]))
+
+
+	var result: String = "The total is %d." % [total]
+	print(result)
+	results_label.text = result
+
+
+func mul(op1: int, op2: int) -> int:
+	return op1 * op2
